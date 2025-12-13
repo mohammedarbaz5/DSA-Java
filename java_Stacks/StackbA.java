3ca6d5612a243c7ee85732be745c2808f886df93
Mohammed Abdul Arbaz <151992379+mohammedarbaz5@users.noreply.github.com>
2025-12-13 09:31:07 +0530

    Add files via upload

---
+++ java_Stacks/StackbA.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class StackbA{
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+        public static void push(int data){
+            list.add(data);
+        }
+        public static int pop(){
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        public static int peek(){
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            System.out.println("Popped Element: "+s.pop());
+    }
+}
+}
