68d455896ab7e16e8ad91a5a0be5310ef1dc6d89
Mohammed Abdul Arbaz <151992379+mohammedarbaz5@users.noreply.github.com>
2025-12-19 21:57:48 +0530

    Update TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -14,13 +14,12 @@ public class TwoSum {
 
     public static void main(String[] args) {
 
-        // Example input
         int[] nums = {2, 7, 11, 15};
         int target = 18;
 
         int[] result = twoSum(nums, target);
 
-        // Output
         System.out.println("Output indices: [" + result[0] + ", " + result[1] + "]");
     }
 }
+
